ff9e7ee7eac7cc3efdf2fcfaaa20d52d1063f443
Константин Худышка <k.iroshnikov@itmegastar.com>
2021-02-02 13:50:32 +0700

    начинаю Stateful виджеты

--- lib/main.dart
+++ lib/main.dart
@@ -9,53 +9,41 @@ class MyfirstApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: Text("My first app"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-            LinearProgressIndicator(value: 23),
-            Text(
-              '23 %',
-              style: TextStyle(color: Colors.white, fontSize: 20),
+            appBar: AppBar(
+              title: Text("My first app"),
+              centerTitle: true,
             ),
-            Text(
-              'Press button to dowload',
-              style: TextStyle(color: Colors.white, fontSize: 20),
+            body: CenterContent(),
+            backgroundColor: Colors.indigo,
+            floatingActionButton: FloatingActionButton(
+              onPressed: null,
+              child: Icon(Icons.cloud_download),
             )
-          ]),
-        ),
-      ),
-      backgroundColor: Colors.indigo,
-    ));
+          )
+        );
   }
 }
 
-
-
-// class CenterContent extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//         child: Container(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: <Widget>[
-//             LinearProgressIndicator(value: 23),
-//             Text(
-//               '23 %',
-//               style: TextStyle(color: Colors.white, fontSize: 20),
-//             ),
-//             Text(
-//               'Press button to dowload',
-//               style: TextStyle(color: Colors.white, fontSize: 20),
-//             )
-//           ]),
-//         ),
-//       );
-//   }
-// }
+class CenterContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        padding: EdgeInsets.all(16), //отступы по сторонам
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              LinearProgressIndicator(value: 23),
+              Text(
+                '23 %',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              Text(
+                'Press button to dowload',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              )
+            ]),
+      ),
+    );
+  }
+}
